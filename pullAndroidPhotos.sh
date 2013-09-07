9bfdb626ec5bb06d8bef3e4e9261244d6ddf4844
Hassan Benjamin Shirani <hshirani@zenfactory.org>
2013-09-07 02:47:36 -0500

    - Added variables for directories for easir configuration - Added comments - Added check to make sure adb is installed

--- pullAndroidPhotos.sh
+++ pullAndroidPhotos.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+###
+# Make sure adb is installed
+###
+sudo apt-get install android-tools-adb
+
+###
+# Define image locationes
+###
+
+# Where do you keep all your photos
+imgRoot="/home/`whoami`/Pictures"
+
+# A new directory based on the current unix epoch to store this image pull
 imgDir=`date +%s`
-mkdir /home/`whoami`/Pictures/$imgDir
-adb pull /storage/sdcard0/DCIM/Camera/ /home/`whoami`/Pictures/$imgDir
+
+# The path of the images on your android phone
+cameraDir="/storage/sdcard0/DCIM/Camera/"
+
+# Create the directory to store the images in
+mkdir $imgRoot/$imgDir
+
+# Pull images from android device to directory created above
+adb pull $cameraDir $imgRoot/$imgDir
