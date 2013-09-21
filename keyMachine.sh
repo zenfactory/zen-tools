7e000865287b3684ea005158ba6e388ffb8a955e
Hassan Benjamin Shirani <hshirani@zenfactory.org>
2013-09-21 03:37:20 -0400

    - updated keyMachine to include ssh port

--- keyMachine.sh
+++ keyMachine.sh
@@ -2,14 +2,15 @@
 
 # Check to make sure we have everything we need
 echo "$#"
-if [ "$#" == 3 ]
+if [ "$#" == 4 ]
 then
 
 	# Setup Vars
-	user=$1
-	host=$2
-	keyFile=$3
+	port=$1
+	user=$2
+	host=$3
+	keyFile=$4
 
 	# Send key to remote host and append it to the authorized key's file
-	cat $keyFile | ssh $user@$host "cat - ~/.ssh/authorized_keys > ~/.ssh/authorized_keys_new && mv ~/.ssh/authorized_keys_new .ssh/authorized_keys"
+	cat $keyFile | ssh -p $3 $user@$host "cat - ~/.ssh/authorized_keys > ~/.ssh/authorized_keys_new && mv ~/.ssh/authorized_keys_new .ssh/authorized_keys"
 fi
