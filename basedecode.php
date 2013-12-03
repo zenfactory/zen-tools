e922950ed5fa0e5895fd3537afd7ff202172626a
hassan <hassan@february.zenfactory.org>
2013-12-02 22:38:26 -0600

    - adding firewall rule scripts to repository

---
+++ basedecode.php
@@ -0,0 +1,6 @@
+<?php
+
+#echo base64_decode("egGIKMpZ6TS6ow9TS");
+echo urldecode("http%3A%2F%2Fjobs.leidos.com%2Fjob%2FAber-Prov-Grd-Information-Security-Engineer-Job-MD%2F22716000%2F&_txt=http%3A%2F%2Fjobs.leidos.com%2Fjob%2FAber-Prov-Grd-Information-Security-Engineer-Job-MD%2F22716000%2F");
+
+?>
