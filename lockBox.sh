5c13087f2f671004f4c71b0dcdf47a8f7040d02d
Hassan Shirani <hshirani@zenfactory.org>
2014-07-30 15:46:23 -0500

    - Added firewall scripts - Added lockdown script - Added rc.local lockdown bootstrap config

--- lockBox.sh
+++ lockBox.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 # Install security tools
-apt-get install psad denyhosts tcpdump chkrootkit rkhunter snort ferm libapache2-modsecurity apache2-suexec libapache2-modsecurity
+apt-get install psad tcpdump chkrootkit rkhunter snort 
 
 # Create security specific logs directory and subdirectories for different log types
 mkdir /root/security/
-mkdir /root/security/chckroot
-mkdir /root/security/dpkg
+mkdir /root/security/logs
+mkdir /root/security/logs/chckroot
+mkdir /root/security/logs/rkhunter
+mkdir /root/security/logs/dpkg
+mkdir /root/security/logs/tcpdump
+mkdir /root/security/logs/snort
 
 # Start tcp dump with rotating logs at boot
-echo "tcpdump -n -C 128 -W 100 -i eth0 -w /root/security/logs/tcpdump/eth0-`date +%s`.pcap &" >> /etc/rc.local
+echo "tcpdump -n -C 500 -W 100 -i eth0 -w /root/security/logs/tcpdump/eth0-`date +%s`.pcap &" >> /etc/rc.local
 
 # Run root kit checkers at boot
 echo "chkrootkit > /root/security/logs/chkroot/chkroot-`date +%s`.txt &" >> /etc/rc.local
 echo "rkhunter --check --cronjob > /root/security/logs/chkroot/rkhunter-`date +%s`.txt &" >> /etc/rc.local
+
+# Start snort at boot
+echo "snort  > /root/security/logs/snort/snort-`date +%s`.txt &" >> /etc/rc.local
 
 # Get list of installed packages at boot (useful to determine if any new packges have been installed). 
 echo "dpkg --get-selections > /root/security/logs/dpkg/installed-packages-`date +%s`.txt &" >> /etc/rc.local
---
+++ lockDown.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Start firewall's now
+/home/hshirani/repo/zen-tools/fw-eth0.fw
+/home/hshirani/repo/zen-tools/fw-usb0.fw
+/home/hshirani/repo/zen-tools/fw-wlan0.fw
+/home/hshirani/repo/zen-tools/fw-wlan1.fw
+
+# Run root kit checkers  now
+chkrootkit > /root/security/logs/chkroot/chkroot-`date +%s`.txt
+rkhunter --check --cronjob > /root/security/logs/chkroot/rkhunter-`date +%s`.txt
+
+# Get list of installed packages now
+dpkg --get-selections > /root/security/logs/dpkg/installed-packages-`date +%s`.txt
+
+# Start tcp dump now
+tcpdump -n -C 500 -W 100 -i eth0 -w /root/security/logs/tcpdump/eth0-`date +%s`.pcap &
+tcpdump -n -C 500 -W 100 -i wlan0 -w /root/security/logs/tcpdump/wlan0-`date +%s`.pcap &
+tcpdump -n -C 500 -W 100 -i wlan1 -w /root/security/logs/tcpdump/wlan1-`date +%s`.pcap &
+tcpdump -n -C 500 -W 100 -i usb0 -w /root/security/logs/tcpdump/usb0-`date +%s`.pcap &
+
+
+# Start snort now
+cd /root/security/logs/snort
+snort -i eth0 -b & 
+snort -i wlan0 -b & 
+snort -i wlan1 -b & 
+snort -i usb0 -b & 
